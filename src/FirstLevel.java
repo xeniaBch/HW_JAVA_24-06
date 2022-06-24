833ee2d6ff2668629369a1b5c196d905df6ff1d4
Xenia Bechthold <kseniabekhtgold@gmail.com>
2022-06-24 14:14:20 +0200

    Refactoring and changes

--- src/FirstLevel.java
+++ src/FirstLevel.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class FirstLevel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Task1");
+        System.out.println("Task1: Write a method that takes a String and a two chars. " +
+                "As the result it returns the given String, where each the first char was replaced by the second char." +
+                " You have to use only length() and charAt() methods of String class.");
         System.out.println("Please enter phrase");
         String phrase = scanner.nextLine();
-        System.out.println("Please enter first char");
+        System.out.println("Please enter first char need to be replaced");
         String firstChar = scanner.nextLine();
-        System.out.println("Please enter second char");
+        System.out.println("Please enter second char that you want to use to replace");
         String secondChar = scanner.nextLine();
-        replaceChar(phrase, firstChar, secondChar);
+        System.out.println(replaceChar(phrase, firstChar, secondChar));
 
         System.out.println();
 
@@ -31,21 +33,21 @@ public class FirstLevel {
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
-        System.out.println(sum);
+        System.out.println("Sum of all array elements is: "+ sum);
     }
 
-    private static void findMaxPosition ( int[] array, int max){
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] == max) {
-                    System.out.printf("Maximum is on position %s", i);
-                }
+    private static void findMaxPosition(int[] array, int max) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == max) {
+                System.out.printf("Maximum is on position %s", i);
             }
         }
+    }
 
     private static int findMax(int[] array) {
-        int max= array[0];
-        for (int i = 0; i < array.length; i++){
-            if (array[i] > max){
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
                 max = array[i];
             }
         }
@@ -57,23 +59,25 @@ public class FirstLevel {
         System.out.println("Enter array length");
         int n = scanner.nextInt();
         int[] array = new int[n];
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             System.out.println("Enter array value: ");
             array[i] = scanner.nextInt();
-            }
+        }
         return array;
     }
 
-    private static void replaceChar(String phrase, String first, String second) {
+    private static String replaceChar(String phrase, String first, String second) {
+        String string = "";
         for (int i = 0; i < phrase.length(); i++) {
             if (phrase.charAt(i) == first.charAt(0)) {
-                System.out.print(second);
+                string = string + second;
             } else {
-                System.out.print(phrase.charAt(i));
+                string = string + phrase.charAt(i);
             }
         }
-    }
+        return string;
 
+    }
 
 
 }
